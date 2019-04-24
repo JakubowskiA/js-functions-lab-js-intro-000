312dee80f0d0419a718c51ac7f3f3ae1ac0a3867
Ariel J <arieljakubowski@gmail.com>
2019-04-24 02:25:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}// write your code below!
+
+function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`;
+}
+
+function happyHolidaysTo(name, holiday) {
+return  `Happy ${holiday}, ${name}!`;
+}
+  
+function holidayCountdown(name, holiday) {
+  return `It's ${days} days until ${holiday}!`;
+}
