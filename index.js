7c6a31efffcc0bc37aae2acea6fbc9febaf67c80
Ariel J <arieljakubowski@gmail.com>
2019-04-24 02:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ return `Happy holidays, ${name}!`;
 function happyHolidaysTo(name, holiday) {
 return  `Happy ${holiday}, ${name}!`;
 }
-  
+
 function holidayCountdown(name, holiday) {
   return `It's ${days} days until ${holiday}!`;
 }
